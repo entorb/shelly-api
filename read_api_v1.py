1ea314cc4bac68bfe6a1c1ec5edc6c17930cc43d
Torben <59419684+entorb@users.noreply.github.com>
2023-10-04 13:06:08 +0200

    Finalize read_api_v2.py data readout

--- read_api_v1.py
+++ read_api_v1.py
@@ -1,5 +1,5 @@
 """
-Access API v1 of Shelly device (Plug S) using basic auth.
+Access API gen1 of Shelly device (Plug S) using basic authentication.
 
 see https://shelly-api-docs.shelly.cloud/gen1/#shelly-plug-plugs-settings
 """
@@ -8,46 +8,54 @@ import json
 
 import requests
 
-from credentials import password, shelly1_ip, username
+from credentials import password, shelly1_ip as shelly_ip, username
 
 # public endpoint with no auth required
 # url = f"http://{shelly_ip}/shelly"
 
 # other endpoints require basic auth, if auth is configured in web interfaces
-# status endpoint, shows much data
+# status endpoint, shows much data, e.g. temperature
 # see https://shelly-api-docs.shelly.cloud/gen1/#shelly-plug-plugs-meter-0
 # url = f"http://{shelly1_ip}/status"
-# read meter data
-# see https://shelly-api-docs.shelly.cloud/gen1/#shelly-plug-plugs-meter-0
-url = f"http://{shelly1_ip}/meter/0"
 
+# read meter data
+# see https://shelly-api-docs.shelly.cloud/gen1/#shelly-plug-plugs-status
+url = f"http://{shelly_ip}/meter/0"
 
 # creating session with http basic auth
 session = requests.Session()
 session.auth = (username, password)
 
 
-def write_data_to_file(data: dict) -> None:
-    with open("data.json", mode="w", encoding="utf-8", newline="\n") as fh:
-        json.dump(data, fh, ensure_ascii=False, sort_keys=False, indent=2)
+# def write_data_to_file(data: dict) -> None:
+#     with open("data.json", mode="w", encoding="utf-8", newline="\n") as fh:
+#         json.dump(data, fh, ensure_ascii=False, sort_keys=False, indent=2)
 
 
 try:
     response = session.get(url, timeout=3)
 
     if response.status_code == 200:
-        # print("API Response:")
         # print(response.text)
         # convert response to dict
         data = json.loads(response.text)
-        # print(data)
+        print(data)
         # write_data_to_file(data)
+
+        # Current real AC power being drawn, in Watts
         watt_now = float(data["power"])
-        kWh_total = float(data["total"])
+        # Total energy consumed by the attached electrical appliance in Watt-minute
+        total = float(data["total"])
+        # Energy counter value for the last 3 round minutes in Watt-minute
         watt_past_minutes = [float(x) for x in data["counters"]]
-        # list of 1-min power averages for last 3 minutes
+        # print(watt_past_minutes)
+        # Timestamp of the last energy counter value, with the applied timezone
+        timestamp = int(data["timestamp"])
+
     else:
-        print(f"Failed to access the API. Status code: {response.status_code}")
+        print(
+            f"Failed to access the API. Status code: {response.status_code}, text: {response.text}",  # noqa: E501
+        )
 
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {str(e)}")
--- read_api_v2.py
+++ read_api_v2.py
@@ -1,21 +1,28 @@
 """
-Access API v2 of Shelly device (Plus Plug S) using basic auth.
+Access API gen2 of Shelly device (Plus Plug S) using digest auth.
 
 see https://shelly-api-docs.shelly.cloud/gen2/General/Authentication
 """
+# import time
+import random
+import json
 
+import hashlib
 import requests
 
-from credentials import shelly2_ip
+from credentials import shelly2_ip as shelly_ip, username, password
 
 # public endpoint with no auth required
 # api_url = f"http://{shelly_ip}/shelly"
 
-url = f"http://{shelly2_ip}/rpc"
+url = f"http://{shelly_ip}/rpc"
+method = "Shelly.GetStatus"
+method = "Switch.GetStatus"
+method_arg = "id=0"
 
-data = {
+payload_401 = {
     "id": 1,
-    "method": "Shelly.GetStatus",
+    "method": method,
 }
 
 
@@ -23,33 +30,100 @@ def extract_data_from_401(response_header: dict) -> dict[str, str | int]:
     """
     Extract data from Shelly 401 response and convert to dict.
     """
-    data_out = {}
+    data_401 = {}
     s = response_header["WWW-Authenticate"]
+    s = s.replace("Digest qop", "qop")
     # remove " from values
     s = s.replace('"', "")
     # extract key-value pairs
     for key_value in s.split(", "):
         (key, value) = key_value.split("=")
-        data_out[key] = value
-    return data_out
+        data_401[key] = value
+    return data_401
 
 
+# 1. request without auth, get a onetime-no and http 401
 try:
-    response = requests.post(url, json=data, timeout=3)
-    # 1. request without auth, get a onetime-no and http 401
+    response = requests.post(
+        url,
+        timeout=3,
+        json=payload_401,
+        # data=json.dumps(payload_401),
+    )
     if response.status_code == 401:
         # print(response.headers)
-        data = extract_data_from_401(response.headers)
-        print(data)
-        nonce = data["nonce"]
-        realm = data["realm"]
+        data_401 = extract_data_from_401(response.headers)
     else:
         print(
             f"Failed to access the API. Status code: {response.status_code}, text: {response.text}",  # noqa: E501
         )
+        exit()
+    # print(data_401)
 
-    # 2. request step 2 using SHA-256
-    # TODO
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {str(e)}")
+    exit()
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+    exit()
+
+
+# 2. request step 2 using SHA-256
+print("Start Step 2")
+try:
+    auth_parts = [username, data_401["realm"], password]
+    # Concatenate the auth_parts with ':' and compute the SHA-256 hash
+    ha1 = hashlib.sha256(":".join(auth_parts).encode()).hexdigest()
+    ha2 = hashlib.sha256(b"dummy_method:dummy_uri").hexdigest()
+    # print(ha1)
+    # print(ha2)
+
+    # cnonce = str(int(time.time()))
+    cnonce = str(random.randint(1000000, 9999999))  # noqa: S311
+
+    nc = "1"  # number, nonce counter (returned only through websocket channel).
+    # It has value of 1 if it is not available in the response
+
+    s = ":".join((ha1, data_401["nonce"], nc, cnonce, "auth", ha2))
+    resp = hashlib.sha256(s.encode()).hexdigest()
+
+    d = {
+        "id": 1,
+        "method": method,
+        "params": {"id": 0},  # 0 = first switch/meter
+        "auth": {
+            "realm": data_401["realm"],
+            "username": username,
+            "nonce": data_401["nonce"],
+            "cnonce": cnonce,
+            "response": resp,
+            "algorithm": "SHA-256",
+        },
+    }
+    response = requests.post(f"http://{shelly_ip}/rpc", json=d, timeout=3)
+    res = json.loads(response.text)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        data = data["result"]
+        print(data)
+
+        # Last measured instantaneous active power (in Watts) delivered to the attached load   # noqa: E501
+        watt_now = float(data["apower"])
+        # Total energy consumed in Watt-hours
+        kWh_total = float(data["aenergy"]["total"])
+        # Energy consumption by minute (in Milliwatt-hours) for the last three minutes
+        past_minutes = [float(x) for x in data["aenergy"]["by_minute"]]
+        # Unix timestamp of the first second of the last minute
+        timestamp = int(data["aenergy"]["minute_ts"])
+        # Temperature in Celsius (null if temperature is out of the measurement range)
+        temp = float(data["temperature"]["tC"])
+
+    else:
+        print(
+            f"Failed to access the API. Status code: {response.status_code}, text: {response.text}",  # noqa: E501
+        )
+        exit()
 
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {str(e)}")
