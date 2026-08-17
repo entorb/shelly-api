282ae23abebee03fcad54a2c854a3cd8faf1bf0a
Torben <59419684+entorb@users.noreply.github.com>
2026-08-17 08:45:53 +0200

    Update credentials.EXAMPLE.py

--- credentials.EXAMPLE.py
+++ credentials.EXAMPLE.py
@@ -4,7 +4,7 @@ Shelly devices credentials example.
 to adjust and rename
 """
 
-shelly1_ip = "192.168.0.1"
-shelly2_ip = "192.168.0.2"
+shelly1_ip = "127.0.0.1"
+shelly2_ip = "127.0.0.1"
 username = "admin"  # always
 password = "xxx"  # noqa: S105
